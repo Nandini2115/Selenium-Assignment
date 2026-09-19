63679c2578d9f6ce0b80a7bf78a3cec10ee8abae
Karuturi Nandini <167072888+Nandini2115@users.noreply.github.com>
2026-09-19 10:25:13 +0530

    Day10_Assignment

---
+++ Day10_Assignment/SauceDemoKeywordFrameworkImplementation/SauceImplementation.java
@@ -0,0 +1,28 @@
+package SauceDemoKeywordFrameworkImplementation;
+
+import SauceDemoKeyword.keywordSauceExecutor;
+
+public class SauceImplementation {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        keywordSauceExecutor e = new keywordSauceExecutor();
+
+        e.executors("LAUNCH_BROWSER");
+        Thread.sleep(1000);
+
+        e.executors("OPEN_URL");
+        Thread.sleep(1000);
+
+        e.executors("USERNAME");
+        Thread.sleep(1000);
+
+        e.executors("PASSWORD");
+        Thread.sleep(1000);
+
+        e.executors("LOGIN");
+        Thread.sleep(1000);
+
+        e.executors("CLOSE_BROWSER");
+    }
+}
